95c78012f7d1b45289101428f57fea5156e9e721
root <root@beaglebone.localdomain>
2018-08-18 13:36:56 +0000

    try 9

--- imu/i2c/readAndSampleData.py
+++ imu/i2c/readAndSampleData.py
@@ -25,7 +25,7 @@ try:
 	#mp2 = mpu9250.SL_MPU9250(0x69,2)
 except:
 	print("IMU's : Failed to import or execute mpu9250 library, IMU is probably not connected rightly")
-fileName = "sampleData400try8"
+fileName = "sampleData400try9"
 sampleFreq = 100
 sampleTime = 10
 
@@ -37,12 +37,12 @@ ctr = 1
 timeout = time.time() + sampleTime
 while True:
 	try:
-		ax1 = "1"
-		ay1 = "1"
-		az1 = "1"
+		#ax1 = "1"
+		#ay1 = "1"
+		#az1 = "1"
 
 		#ax1, ay1, az1 = mp1.getAccel()
-		#gx1, gy1, gz1 = mp1.getGyro()
+		gx1, gy1, gz1 = mp1.getGyro()
 		#print "Eerste IMU values:"
 		#print "Ax1: ",ax1
 		#print "Ay1: ",ay1
@@ -52,7 +52,7 @@ while True:
 		#print "Gy1: ",gy1
 		#print "Gz1: ",gz1
 		#data.write(str(time.time())+str(ax1)+','+str(ay1)+','+str(az1)+','+str(gx1)+','+str(gy1)+','+str(gz1)+'\n')
-		data.write(str(time.time())+str(ax1)+','+str(ay1)+','+str(az1)+'\n')
+		data.write(str(time.time())+str(gx1)+','+str(gy1)+','+str(gz1)+'\n')
 		#time.sleep(float(1)/sampleFreq)
 
 		if time.time()>timeout:
